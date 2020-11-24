4c749b9500a386f20fde95c43aed173e2b6f7e20
maidangkhanh <mai.d.khanh@gmail.com>
2020-11-24 17:51:25 +0700

    Search slang and search definition

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.io.*;
 
@@ -6,7 +8,7 @@ import java.io.*;
 public class Dictionary {
     static HashMap<String, String[]> dict = new HashMap<String, String[]>();
 
-    public static void load(String fileName) throws IOException {
+    public void load(String fileName) throws IOException {
         try{
             BufferedReader bufferedReader = new BufferedReader(new FileReader("slang.txt"));
             String line;
@@ -19,5 +21,30 @@ public class Dictionary {
             e.printStackTrace();
         }
     }
+
+    public String[] searchSlang(String slang){
+        String[] defs = {};
+        if (dict.containsKey(slang))
+        {
+            defs = dict.get(slang);
+        }
+        return defs;
+    }
+
+    public ArrayList<String> searchDefinition(String def){
+        ArrayList<String> slangs = new ArrayList<String>();
+        for(String i:dict.keySet())
+        {
+            String[] values = dict.get(i);
+            for(String j:values)
+            {
+                if(j.equals(def))
+                    slangs.add(i);
+            }
+        }
+        return slangs;
+    }
+
+
 
 }
